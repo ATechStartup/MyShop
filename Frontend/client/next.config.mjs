da5aa17a7dc77fa7e7f6ceb2f7eaeaa8b6557836
AliAfarinifard <aliafarinifard@outlook.com>
2024-12-11 14:44:13 +0330

    next.config updated

--- Frontend/client/next.config.mjs
+++ Frontend/client/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["firebasestorage.googleapis.com", "res.cloudinary.com"],
+        domains: ["res.cloudinary.com"],
     }
 };
 
